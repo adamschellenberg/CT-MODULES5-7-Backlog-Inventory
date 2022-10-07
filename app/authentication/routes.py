513e7689c290a58c196af27d4824e727b01d6c00
Adam Schellenberg <adamschellenberg93@gmail.com>
2022-10-07 09:25:32 -0700

    update auth

--- app/authentication/routes.py
+++ app/authentication/routes.py
@@ -5,3 +5,47 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, LoginManager, current_user, login_required
 
 auth = Blueprint('auth', __name__, template_folder='auth_templates')
+
+@auth.route('/signup', methods = ['GET', 'POST'])
+def signup():
+    form = UserLoginForm
+
+    try:
+        if request.method == 'POST' and form.validate_on_submit():
+            name = form.name.data
+            email = form.email.data
+            password = form.password.data
+            print(name, email, password)
+
+            user = User(name, email, password = password)
+
+            db.session.add(user)
+            db.session.commit()
+
+            flash(f'You have successfully created a user account {name}', 'User-created')
+            return redirect(url_for('site.home'))
+    except:
+        raise Exception('Invalid form data: Please check your form')
+    return render_template('sign_up.html', form=form)
+
+@auth.route('/signin', methods=['GET', 'POST'])
+def signin():
+    form = UserLoginForm()
+
+    try:
+        if request.method == 'POST' and form.validate_on_submit():
+            name = form.name.data
+            email = form.email.data
+            password = form.password.data
+            print(name, email, password)
+
+            logged_user = User.query.filter(User.email == email).first()
+            if logged_user and check_password_hash(logged_user.password, password):
+                login_user(logged_user)
+                flash('You logged in!', 'auth-success')
+                return redirect(url_for('site.backlog'))
+            else:
+                flash("I'm sorry, Dave. I'm afraid I can't let you in.", 'auth-failed')
+    except:
+        raise Exception('Invalid form data: Please check your form')
+    return render_template('sign_in.html', form=form)
